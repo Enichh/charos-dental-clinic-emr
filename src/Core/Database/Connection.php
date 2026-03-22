39b39724b0e259554ebf618fe020bddef33c335d
Enichh <enochastor@gmail.com>
2026-03-22 14:57:48 +0800

    Added Database connection

--- src/Core/Database/Connection.php
+++ src/Core/Database/Connection.php
@@ -1,18 +1,24 @@
 <?php
-require 'vendor/autoload.php'
 
-use PDO; 
-$dotenv = new Dotenv(__DIR__);
+use PDO;
+require dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
 $dotenv->load();
 
-$host = getenv('DB_HOST'); 
-$db_name = getenv('DB_NAME');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
 
-try{
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password
-}
-catch(PDOException $e){
+$host     = getenv('DB_HOST');
+$db_name  = getenv('DB_NAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');  
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
     echo "Connection error: " . $e->getMessage();
 }
